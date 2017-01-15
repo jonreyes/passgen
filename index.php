41c5604a23b8a0db49f86783db3287708ae6aa6c
Jon <jonrey3@gmail.com>
2017-01-14 22:56:23 -0500

    Allow config

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <?php
-	$lower = implode(range('a','z'));
-	$upper = implode(range('A','Z'));
+function rand_char($string){
+	$i = random_int(0,strlen($string)-1);
+	return $string[$i];
+}
+
+function rand_string($length, $char_set){
+	$output = '';
+	for($i=0; $i < $length; $i++){
+		$output .= rand_char($char_set);
+	}	
+	return $output;
+}
+
+function generate_password($length){
+	$low = implode(range('a','z'));
+	$up = implode(range('A','Z'));
 	$num = implode(range('0','9'));
 	$sym = '$*?!-';
-	$chars = $lower.$upper.$num.$sym;
 	
-	function rand_char($string){
-		$i = random_int(0,strlen($string)-1);
-		return $string[$i];
-	}
-	
-	echo rand_char($chars);
+	$use_low = true;
+	$use_up = true;
+	$use_num = true;
+	$use_sym = true;
+
+	$chars = '';
+
+	if($use_low === true){$chars .= $low;}
+	if($use_up === true){$chars .= $up;}	
+	if($use_num === true){$chars .= $num;}
+	if($use_sym === true){$chars .= $sym;}
+
+	return rand_string($length,$chars);
+}
+
+echo generate_password(16);
 ?>
